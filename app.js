2cdd51a935039707f636497b39bbe69456620aef
alioshka <macarena7777777@gmail.com>
2024-05-30 14:50:11 +0300

    Операторы присваивания и сравнения

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
-// Базовые арифметические операторы
+// Операторы присваивания
+let age = 18 + 5;
+age += 3; // age = age + 2
+age -= 3;
+age *= 2;
+age /= 2;
 
-/* 
-    две
-    строки
-*/
+age++; // age = age + 1
+age--;
+console.log(age);
 
-const width = 10;
-const height = 5;
-const space = width * height;
-const newWidth = width - 4;
-const newWidth2 = width + 4;
-const division = newWidth / newWidth2;
-const volume = 2 ** 3; // 2 * 2 * 2
-console.log(division);
-
-
-// Строки
-const city = 'Москва';
-const street = 'Nовослободская';
-console.log(city + ', ' + street + ' ' + 5)
+// Операторы сравнения
+const vasia = 20;
+console.log(age > vasia);
+console.log(age >= vasia);
+console.log(age < vasia);
+console.log(age <= vasia);
+console.log(age == vasia);

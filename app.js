8094683bc41d55aa6f1b2215f131132289cfaaa8
alioshka <macarena7777777@gmail.com>
2024-05-30 14:43:31 +0300

    Арифметические операторы

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-let a = 1;
-console.log(a);
-a = 'test';
-console.log(a);
+// Базовые арифметические операторы
+
+/* 
+    две
+    строки
+*/
+
+const width = 10;
+const height = 5;
+const space = width * height;
+const newWidth = width - 4;
+const newWidth2 = width + 4;
+const division = newWidth / newWidth2;
+const volume = 2 ** 3; // 2 * 2 * 2
+console.log(division);
+
+
+// Строки
+const city = 'Москва';
+const street = 'Nовослободская';
+console.log(city + ', ' + street + ' ' + 5)

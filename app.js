fc3f262bd1098c6a93dcc27e2519593889fd4935
alioshka <macarena7777777@gmail.com>
2024-05-30 17:23:03 +0300

    False из других типов

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
-const projectName = 'cайт магазина'
-const price = 2000;
-const author = 'Василий Пупкин';
-
-const template = author + ' заказал ' + projectName + ' по цене ' + price + '$'
-console.log(template);
-
-const template2 = `${author} заказал ${projectName} по цене ${price}$`;
-console.log(template2);
-
-const template3 = 'Проект\nЦена: ' + price + '$';
-console.log(template3);
-
-const template4 = `Проект
-Цена: ${price}$`;
-console.log(template4);
-
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(Number('sx')));
+console.log(Boolean(NaN));
